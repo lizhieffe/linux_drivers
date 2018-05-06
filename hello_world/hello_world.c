45b70bda5dabdd2b2c40847668483db225567da3
lizhi <lizhieffe@gmail.com>
2018-05-06 14:54:49 -0700

    add hello world driver

---
+++ hello_world/hello_world.c
@@ -0,0 +1,23 @@
+#include <linux/init.h>
+#include <linux/module.h>
+#include <linux/kernel.h>
+
+static void print_log(void);
+
+static int __init hello_world_init(void) {
+  print_log();
+  return 0;
+}
+
+static void __exit hello_world_exit(void) {
+  pr_info("End of Hello world!\n");
+}
+
+static void print_log(void) {
+  pr_info("Hello worldi!\n");
+}
+
+module_init(hello_world_init);
+module_exit(hello_world_exit);
+MODULE_AUTHOR("lizhi");
+MODULE_LICENSE("GPL");
